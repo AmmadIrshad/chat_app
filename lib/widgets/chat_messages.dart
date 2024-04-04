0d3317a0b42fdac24fe00a8bdb0b25d5a5a51c87
ammad irshad <itzammadirshad@gmail.com>
2024-04-05 02:58:43 +0500

    loading_displaying_chat_messages_as_stream

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +6,36 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No messages found!!!'),
-    );
+    //snapshot will yield such a stream
+    //stream----> setup a listner which automatically listens to this remote database their to the chat collection
+    //when new document added here it will automatically notify and trigger builder function
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('createdAt', descending: false)
+            .snapshots(),
+        builder: (ctx, chatSnapshots) {
+          if (chatSnapshots.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          //empty list
+          if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+            const Center(
+              child: Text('No messages found!!!'),
+            );
+          }
+          if (!chatSnapshots.hasError) {
+            const Center(
+              child: Text('Something went wrong'),
+            );
+          }
+
+          final loadedMessages = chatSnapshots.data!.docs;
+          return ListView.builder(
+              itemCount: loadedMessages.length,
+              itemBuilder: (ctx, index) {
+                return Text(loadedMessages[index].data()['text']);
+              });
+        });
   }
 }
